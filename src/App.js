81d0c81c9fab3baae105198bb0e9dbb430425567
nuclearWintersru <armandonarcizoruedaperez@gmail.com>
2019-06-29 13:08:18 -0500

    change3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      nombres: [],
       turno: 0
     }
     this.cambiarTurno = this.cambiarTurno.bind(this)
@@ -15,13 +16,33 @@ class App extends Component {
     this.setState({turno: this.state.turno + numero})
   }
 
+  //shouldComponentUpdate(nextProps, nextState) {
+  //  console.log("should next? ", nextState)
+  //  console.log("should this? ", this.state)
+  //  return this.state.nombres !== nextState.nombres || this.state.turno !== nextState.turno
+  //}
+
+  componentDidMount () {
+    console.log("Activado solo una vez")
+    fetch('http://192.168.1.66:3001/ejemplo')
+    .then((data) => data.json())
+    .then((json) => this.setState({nombres: json}))
+  }
+
   render () {
-    console.log("Props App ", this.props)
-    console.log("State App", this.state)
+    //console.log("Props App ", this.props)
+    //console.log("State App", this.state)
+    console.log("Se volvio a renderizar ", this.state.nombres)
     return (
       <div style={{alignItems: "center", display: "flex", flexDirection: "column" }}>
         <div>{this.state.turno}</div>
         <Botones nombre="Armando" funcionTurno = {this.cambiarTurno.bind(this)} />
+        {this.state.nombres.map((nom, i) => 
+          <div key={i}>
+            <div>{nom.nombre}</div>
+            <div>hola!</div>
+          </div>
+        )}
       </div>
     );
   }
--- src/componentes/botones.js
+++ src/componentes/botones.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Botones extends Component {
     state = {}
     render () {
-        console.log("Props Botones ", this.props)
-        console.log("State Botones", this.state)
+        //console.log("Props Botones ", this.props)
+        //console.log("State Botones", this.state)
         return (
             <div>
                 <button onClick={() => this.props.funcionTurno(-1)}>Disminuye un turno</button>
